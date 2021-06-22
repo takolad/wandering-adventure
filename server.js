23e551543f83fd668b1a31626d909253845d350e
JER.8789 <je.rogers8789@gmail.com>
2021-06-21 21:19:20 -0500

    ...

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const sequelize = require("mongoose");
+const sequelize = require("./connection");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
