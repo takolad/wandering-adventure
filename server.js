4d206fe2811c873fd5ff894e039ab3c0d47e7baf
Madison Vega <madison.r.vega@gmail.com>
2021-06-23 21:41:10 -0500

    added scripts to deploy to heroku, installed some more packages

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-
 const sequelize = require("./connection");
 
 const routes = require("./routes");
@@ -14,9 +13,13 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 // Add routes, both API and view
 app.use(routes);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 // Turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
