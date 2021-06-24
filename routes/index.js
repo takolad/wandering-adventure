b69a3a471c128ae0a3e69405bae03f3af9a02f71
JER.8789 <je.rogers8789@gmail.com>
2021-06-23 22:06:19 -0500

    homepage route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const characterRoutes = require('./character');
 const userRoutes = require('./user');
+const homeRoutes = require('./homepage')
 
+router.use('/homepage', homeRoutes);
 router.use('/character', characterRoutes);
 router.use('/user', userRoutes);
 
