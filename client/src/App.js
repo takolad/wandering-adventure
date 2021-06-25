b0884ee9d096fbd0314cd86d9bc2e4c38bac06e4
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-06-24 20:52:55 -0500

    router set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import HomeLog from './pages/Homepage/Homepage'
 import NavBar from './components/Navbar/Navbar'
-import {BrowserRouter, Route} from 'react-router-dom'
 import Game from './components/Game/Game'
+import { BrowserRouter, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <Route path="/" component={HomeLog} />
-      <Route path="/game" component={Game} />
+      <div className="App">
+      <NavBar/>
+      <Route path="/" component={HomeLog}/>
+      <Route path="/game" component={Game}/>
     </div>
     </BrowserRouter>
   );
