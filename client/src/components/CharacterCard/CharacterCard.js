947fc2d722f5e20303b33d372e98c4acce1c97a5
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-06-26 12:45:56 -0500

    Updated CharacterCard

--- client/src/components/CharacterCard/CharacterCard.js
+++ client/src/components/CharacterCard/CharacterCard.js
@@ -15,7 +15,7 @@ export default function MediaCard() {
     <Card>
       <CardActionArea>
         <CardMedia
-          image="https://c10.patreonusercontent.com/3/eyJxIjoxMDAsIndlYnAiOjB9/patreon-media/p/post/52645893/8eb208ae2c2642428f70195a31095da5/1.png?token-time=1625788800&token-hash=wD3C3iyQIVP6oCbboNKu0hPtjhipAxFZUvB-VDpem9c%3D"
+          image="./AATROXBloodMoon.png"
           title="Contemplative Reptile"
         />
         <CardContent>
@@ -23,8 +23,7 @@ export default function MediaCard() {
             Character Name
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica
+            From the depths the King of the Centaur's Dvorak has risen. 
           </Typography>
         </CardContent>
       </CardActionArea>
