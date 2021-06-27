3f1020a56adfd76d44f4d5fe7e0a1be353d124c0
Madison Vega <madison.r.vega@gmail.com>
2021-06-27 10:59:27 -0500

    made changes to formatting

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -12,6 +12,7 @@ import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card';
+
 import './homepage.css'
 
 const useStyles = makeStyles((theme) => ({
@@ -130,6 +131,7 @@ export default function HomeLog() {
                                     >
                                         Log In
                                     </Button>
+                                    
                                 </Container>
                         </div>
                         </div>
