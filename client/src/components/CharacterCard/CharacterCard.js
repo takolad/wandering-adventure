d9a0deb85f67605c3f25a24a60e478885b67449a
Gustavo Gonzalez <gustavoshadow13@gmail.com>
2021-06-28 18:22:46 -0500

    Edits to Game layout

--- client/src/components/CharacterCard/CharacterCard.js
+++ client/src/components/CharacterCard/CharacterCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
+import { makeStyles } from '@material-ui/core/styles';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
@@ -11,14 +12,26 @@ import Typography from '@material-ui/core/Typography';
 
 export default function MediaCard() {
 
+  const useStyles = makeStyles({
+    root: {
+    width: "406.5px",
+    height: "698.18px",
+    left: "34.52px",
+    top: "130.41px"
+    },
+  });
+  
+  const classes = useStyles()
+
   return (
-    <Card>
+
+    <Card className={classes.root}>
       <CardActionArea>
         <CardMedia
           id="character"
           component="img"
-          height="150"
-          image="./Jiraiya.jpg"
+          // height="150"
+          image="https://i.pinimg.com/474x/d0/90/08/d090082483f4dfe04ace7fdc07e3fe4c.jpg"
           title="Contemplative Reptile"
         />
         <CardContent>
@@ -26,7 +39,7 @@ export default function MediaCard() {
             Character Name
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            From the depths the King of the Centaur's Dvorak has risen. 
+            Character Bio: From the depths the King of the Centaur's Dvorak has risen. 
           </Typography>
         </CardContent>
       </CardActionArea>
--- client/src/components/Game/Game.js
+++ client/src/components/Game/Game.js
@@ -1,15 +1,26 @@
 import React, {useEffect, useState} from 'react';
-// import Navbar from '../Navbar/Navbar';
+import Navbar from '../Navbar/Navbar';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-import Card from '../CharacterCard/CharacterCard'
+import Card from '../CharacterCard/CharacterCard';
+import { makeStyles } from '@material-ui/core/styles';
 import './game.css'
 
 const Game = () => {
     const [userState, setUserState] = useState('');
-    const [compState, setCompState] = useState('');
-    
+    const [compState, setCompState] = useState('');   
 
+    const useStyles = makeStyles({
+        root: {
+            width: "862px",
+            // height: "648px",
+            left: "504px",
+            top: "158px",
+            float: "right",
+        }
+    });
+
+    const classes = useStyles()
     
     const compare = (user) => {
         const choices = ["rock", "paper", "scissors"];
@@ -43,8 +54,8 @@ const Game = () => {
     return (
         <div>
         <Card/>
-        <Box>
-            <Box>
+        <Box component= "div" display="inline"className={classes.root}>
+            <Box component= "div" justifyContent="center">
             <Button id='Btn' onClick={ () => setUserState('rock')}>Rock</Button>
             <Button id='Btn' onClick={ () => setUserState('paper')}>Paper</Button>
             <Button id='Btn' onClick={ () => setUserState('scissors')}>Scissors</Button>
