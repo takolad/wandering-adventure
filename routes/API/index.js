d7c7e953e8a38327fe842d63ed9ad0303b93424c
John Hinojosa <hinojohn@ymail.com>
2021-06-30 16:34:37 -0500

    added /random route to event.js (OG code from Jeremiah), updated api index route to handle /api/events, removed commented out withAuth use from character route file, added missing path requirement in server.js

--- routes/API/index.js
+++ routes/API/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
 const gameRoutes = require("./game");
+const eventRoutes = require("./event");
 const characterRoutes = require("./character");
 
 router.use("/users", userRoutes);
 router.use("/games", gameRoutes);
+router.use("/events", eventRoutes);
 router.use("/character", characterRoutes);
 
 module.exports = router;
