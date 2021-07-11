d4d100f504ea3dc8f1f581b2e56ca5023bb0b076
Madison Vega <madison.r.vega@gmail.com>
2021-07-10 21:06:43 -0500

    added more info to readme

--- client/src/pages/SaveSelect/SaveSelect.js
+++ client/src/pages/SaveSelect/SaveSelect.js
@@ -2,20 +2,18 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
+// import CardMedia from '@material-ui/core/CardMedia';
+// import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import FormField from '../../components/FormField';
-import SelectButton from '../../components/SelectButton';
 import { useAuth0 } from '@auth0/auth0-react'
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import API from '../../utils/API'
-import './characterselect.css';
+import './saveselect.css';
 
-export default function CharacterSelect() {
-    const history = useHistory();
+export default function SaveSelect() {
+    // const history = useHistory();
     const { user } = useAuth0();
+
     const userId = user.sub.split("|")[1];
 
     const useStyles = makeStyles({
@@ -27,32 +25,19 @@ export default function CharacterSelect() {
         },
     });
     const classes = useStyles();
-    const [formField, setFormField] = useState({
-        name: '',
+
+    const [charState, setCharState] = useState({
+        character: [] 
+
     })
-    function handleInputChange(event) {
-        const { name, value } = event.target;
-        setFormField({ ...formField, name: value })
-    };
-    function formSubmit(event, charClass) {
-        event.preventDefault();
-        console.log(formField);
-        console.log(charClass);
-        if (formField.name) {
-            API.createCharacter(
-                userId,
-                {
-                    name: formField.name,
-                    class: charClass
-                }
-            )
-                .then(res => {
-                    history.push('/game')
-                })
-        }
+    // do use effect inside use effect api call to get characters, charState.character.map
+    // async function selectSave() {
+    //     const characterArray = await API.getCharacters(userId);
+    //     return characterArray;
 
-    }
-
+    // }
+    // const characters = selectSave();
+    // console.log(characters);
     return (
 
         <div>
@@ -60,47 +45,21 @@ export default function CharacterSelect() {
                 <Grid item sm={2} />
                 <Grid item sm={3}>
                     <Card className={classes.root}>
-                        <CardMedia
-                            className={classes.media}
-                            image="https://live.staticflickr.com/65535/51297013113_71c5d66e7d_w.jpg"
-                            title="https://www.deviantart.com/chaosringen/art/Swordman-536325555"
-                        />
                         <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Warrior
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                A fierce swordsman on a quest to become the greatest knight.
-                            </Typography>
+                            <div>
+                                {charState.character.map(char => {
+                                    return (
+                                        <div key={char.id}></div>
+                                    )
+                                })}
+                            </div>
+
                         </CardContent>
 
                     </Card>
                 </Grid>
-                <Grid item sm={2}>
-                    <Card className={classes.root}>
-                        <CardContent>
-                            <h2>Choose your character and add a custom name!</h2>
-                        </CardContent>
-
-                    </Card>
-                </Grid>
-                <Grid item sm={3}>
-                    <Card className={classes.root}>
-                        <CardMedia
-                            className={classes.media}
-                            image="https://live.staticflickr.com/65535/51296892783_ff5dc2707f_w.jpg"
-                            title="https://www.deviantart.com/gerryarthur/art/Leaena-573272592"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Mage
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                A cunning mage, setting out on their first quest out of their apprenticeship.
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
+                <Grid item sm={2} />
+                <Grid item sm={3} />
                 <Grid item sm={2} />
             </Grid>
         </div>
