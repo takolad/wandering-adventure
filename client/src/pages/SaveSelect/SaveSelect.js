591851e4d77c52aa8249af6b3334e6f33797e9e4
Madison Vega <madison.r.vega@gmail.com>
2021-07-12 21:17:12 -0500

    cleaning up saveselect page code

--- client/src/pages/SaveSelect/SaveSelect.js
+++ client/src/pages/SaveSelect/SaveSelect.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-// import { useHistory } from 'react-router-dom';
 import './saveselect.css';
 import SaveList from '../../components/SaveList';
 
 export default function SaveSelect() {
-    // const history = useHistory();
     const useStyles = makeStyles({
         root: {
             maxWidth: 400,
@@ -21,16 +17,6 @@ export default function SaveSelect() {
     });
     const classes = useStyles();
 
-    
-
-    // do use effect inside use effect api call to get characters, charState.character.map
-    // async function selectSave() {
-    //     const characterArray = await API.getCharacters(userId);
-    //     return characterArray;
-
-    // }
-    // const characters = selectSave();
-    // console.log(characters);
     return (
 
         <div>
