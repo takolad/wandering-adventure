4b493cf35fd89d10cc3e9428c9d9e541fcc6f1fe
John Hinojosa <hinojohn@ymail.com>
2021-07-15 23:51:35 -0500

    CharacterSelect screen now has one select button, SaveList Link altered to match frontend route in App.js

--- client/src/components/SaveList.js
+++ client/src/components/SaveList.js
@@ -1,108 +1,108 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom';
-import DeleteIcon from '@material-ui/icons/Delete';
-import API from '../utils/API';
-import { useAuth0 } from '@auth0/auth0-react'
-import Button from '@material-ui/core/Button'
-import mage from '../pages/CharacterSelect/mage.jpeg'
-import warrior from '../pages/CharacterSelect/warrior.jpeg'
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
+import DeleteIcon from "@material-ui/icons/Delete";
+import API from "../utils/API";
+import { useAuth0 } from "@auth0/auth0-react";
+import Button from "@material-ui/core/Button";
+import mage from "../pages/CharacterSelect/mage.jpeg";
+import warrior from "../pages/CharacterSelect/warrior.jpeg";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        maxWidth: 752,
-    },
-    demo: {
-        backgroundColor: theme.palette.background.paper,
-    },
-    title: {
-        margin: theme.spacing(4, 0, 2),
-        textAlign: 'center'
-    },
+  root: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+    textAlign: "center",
+  },
 }));
 
 export default function SaveList() {
-    const [dense, setDense] = useState(false);
-    const [secondary, setSecondary] = useState(false);
-    const [charState, setCharState] = useState({
-        character: []
+  const [dense, setDense] = useState(false);
+  const [secondary, setSecondary] = useState(false);
+  const [charState, setCharState] = useState({
+    character: [],
+  });
+  const { user } = useAuth0();
+  const userId = user.sub.split("|")[1];
 
-    })
-    console.log(charState.character)
-    useEffect(() => {
-        loadChar()
-    }, [])
-    function loadChar() {
-        API.getCharacters(userId)
-            .then(res => {
-                setCharState({ ...charState, character: res.data })
-                console.log(res.data)
-            })
-            .catch(err => console.log(err))
-    }
+  useEffect(() => {
+    loadChar();
+  }, []);
 
-    function deleteChar(id, user_id) {
-        API.deleteCharacter(user_id, id)
-            .then(res => loadChar())
-            .catch(err => console.log(err));
+  function loadChar() {
+    API.getCharacters(userId)
+      .then((res) => {
+        setCharState({ ...charState, character: res.data });
+      })
+      .catch((err) => console.log(err));
+  }
 
-    }
+  function deleteChar(id, user_id) {
+    API.deleteCharacter(user_id, id)
+      .then((res) => loadChar())
+      .catch((err) => console.log(err));
+  }
 
-    function charID() {
-        console.log(charState.character[0].id)
-    };
+  const classes = useStyles();
 
-    const classes = useStyles();
-
-    const { user } = useAuth0();
-    const userId = user.sub.split("|")[1];
-
-    return (
-        <div className={classes.root}>
-            <Grid>
-                <Grid item xs={12} md={8}>
-                    <Typography variant="h6" className={classes.title}>
-                        Pick your character to continue!
-                    </Typography>
-                    <div className={classes.demo}>
-                        <List dense={dense}>
-                            {charState.character.map(char => {
-                                return (
-                                    <ListItem key={char.id}>
-                                        <Link to={`/game/${char.game.id}/user/${char.user_id}/character/${char.id}`}>
-                                            <Button onClick={() => charID()}>
-                                                <ListItemAvatar>
-                                                    {(char.class === 'Mage' ? <Avatar src={mage} /> : <Avatar src={warrior} />)}
-                                                </ListItemAvatar>
-                                                <ListItemText
-                                                    primary={char.name}
-                                                    secondary={secondary ? 'Secondary text' : null}
-                                                />
-                                            </Button>
-                                        </Link>
-                                        <ListItemSecondaryAction>
-                                            <IconButton edge="end" aria-label="delete" onClick={() => deleteChar(char.id, char.user_id)}>
-                                                <DeleteIcon />
-                                            </IconButton>
-                                        </ListItemSecondaryAction>
-                                    </ListItem>
-                                )
-                            })}
-                        </List>
-                    </div>
-                </Grid>
-            </Grid>
-        </div >
-    );
+  return (
+    <div className={classes.root}>
+      <Grid>
+        <Grid item xs={12} md={8}>
+          <Typography variant="h6" className={classes.title}>
+            Pick your character to continue!
+          </Typography>
+          <div className={classes.demo}>
+            <List dense={dense}>
+              {charState.character.map((char) => {
+                return (
+                  <ListItem key={char.id}>
+                    <Link to={`/character/${char.id}/user/${char.user_id}`}>
+                      <Button>
+                        <ListItemAvatar>
+                          {char.class === "Mage" ? (
+                            <Avatar src={mage} />
+                          ) : (
+                            <Avatar src={warrior} />
+                          )}
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={char.name}
+                          secondary={secondary ? "Secondary text" : null}
+                        />
+                      </Button>
+                    </Link>
+                    <ListItemSecondaryAction>
+                      <IconButton
+                        edge="end"
+                        aria-label="delete"
+                        onClick={() => deleteChar(char.id, char.user_id)}
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-
--- client/src/pages/CharacterSelect/CharacterSelect.js
+++ client/src/pages/CharacterSelect/CharacterSelect.js
@@ -1,145 +1,133 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
-import FormField from '../../components/FormField';
-import SelectButton from '../../components/SelectButton';
-import { useAuth0 } from '@auth0/auth0-react'
-import { useHistory } from 'react-router-dom';
-import API from '../../utils/API'
-import './characterselect.css';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import FormField from "../../components/FormField";
+import SelectButton from "../../components/SelectButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useHistory } from "react-router-dom";
+import API from "../../utils/API";
+import "./characterselect.css";
 
 export default function CharacterSelect() {
-    const history = useHistory();
-    const { user } = useAuth0();
-    const userId = user.sub.split("|")[1];
+  const history = useHistory();
+  const { user } = useAuth0();
+  const userId = user.sub.split("|")[1];
 
-    const useStyles = makeStyles({
-        root: {
-            maxWidth: 400,
-        },
-        media: {
-            height: 400,
-        }
-    });
-   
-    const classes = useStyles();
-    const [formField, setFormField] = useState({
-        name: '',
-    })
-    function handleInputChange(event) {
-        const { name, value } = event.target;
-        setFormField({ ...formField, name: value })
-    };
-    function formSubmit(event, charClass) {
-        event.preventDefault();
-        console.log(formField);
-        console.log(charClass);
-        if (formField.name) {
-            API.createCharacter(
-                userId,
-                {
-                    name: formField.name,
-                    class: charClass
-                }
-            )
-                .then(res => {
-                    console.log(res);
-                    const charId = res.data.id
-                    API.createGame(res.data.id)
-                    .then(res => {
-                        history.push(`/game/${res.data.id}/user/${userId}/character/${charId}`)
-                    })
-                    history.push('/game')
-                })
-        }
-
+  const useStyles = makeStyles({
+    root: {
+      maxWidth: 400,
+    },
+    media: {
+      height: 400,
+    },
+  });
+  const classes = useStyles();
+  const [formField, setFormField] = useState({});
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormField({ ...formField, [name]: value });
+  }
+  function formSubmit(event, charClass) {
+    event.preventDefault();
+    if (formField.warrior && formField.mage) {
+      setFormField({ warrior: "", mage: "" });
+      return;
     }
+    if (formField.warrior) {
+      API.createCharacter(userId, {
+        name: formField.warrior,
+        class: "Warrior",
+      }).then((res) => {
+        const charId = res.data.id;
+        API.createGame(res.data.id).then((res) => {
+          history.push(`/character/${charId}/user/${userId}`);
+        });
+      });
+    } else if (formField.mage) {
+      API.createCharacter(userId, {
+        name: formField.mage,
+        class: "Mage",
+      }).then((res) => {
+        const charId = res.data.id;
+        API.createGame(res.data.id).then((res) => {
+          history.push(`/character/${charId}/user/${userId}`);
+        });
+      });
+    }
+  }
 
-    return (
-
-        <div>
-            <Grid container spacing={2}>
-                <Grid item sm={2} />
-                <Grid item sm={3}>
-                    <Card className={classes.root}>
-                        <CardMedia
-                            className={classes.media}
-                            image="https://live.staticflickr.com/65535/51297013113_71c5d66e7d_w.jpg"
-                            title="https://www.deviantart.com/chaosringen/art/Swordman-536325555"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Warrior
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                A fierce swordsman on a quest to become the greatest knight.
-                            </Typography>
-                        </CardContent>
-                        <Container>
-                            <FormField
-                                onChange={handleInputChange}
-                                name='warrior'
-                                value={formField.name}
-
-                            />
-                            <SelectButton
-                                disabled={!(formField.name)}
-                                onClick={(event) => formSubmit(event, 'warrior')}
-
-                            />
-                        </Container>
-
-                    </Card>
-                </Grid>
-                <Grid item sm={2}>
-                    <Card className={classes.root}>
-                        <CardContent>
-                            <h2>Choose your character and add a custom name!</h2>
-                        </CardContent>
-
-                    </Card>
-                </Grid>
-                <Grid item sm={3}>
-                    <Card className={classes.root}>
-                        <CardMedia
-                            className={classes.media}
-                            image="https://live.staticflickr.com/65535/51296892783_ff5dc2707f_w.jpg"
-                            title="https://www.deviantart.com/gerryarthur/art/Leaena-573272592"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2">
-                                Mage
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                A cunning mage, setting out on their first quest out of their apprenticeship.
-                            </Typography>
-                        </CardContent>
-                        <Container>
-                            <FormField
-                                onChange={handleInputChange}
-                                name='mage'
-                                value={formField.name}
-                            />
-                            <SelectButton
-                                disabled={!(formField.name)}
-                                onClick={(event) => formSubmit(event, 'mage')}
-                            />
-                        </Container>
-                    </Card>
-                </Grid>
-                <Grid item sm={2} />
-            </Grid>
-        </div>
-    );
-};
-
-
-
-
-
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item sm={2} />
+        <Grid item sm={3}>
+          <Card className={classes.root}>
+            <CardMedia
+              className={classes.media}
+              image="https://live.staticflickr.com/65535/51297013113_71c5d66e7d_w.jpg"
+              title="https://www.deviantart.com/chaosringen/art/Swordman-536325555"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Warrior
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                A fierce swordsman on a quest to become the greatest knight.
+              </Typography>
+            </CardContent>
+            <Container>
+              <FormField
+                onChange={handleInputChange}
+                name="warrior"
+                value={formField.name}
+              />
+            </Container>
+          </Card>
+        </Grid>
+        <Grid item sm={2}>
+          <Card className={classes.root}>
+            <CardContent>
+              <h2>Choose your character and add a custom name!</h2>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item sm={3}>
+          <Card className={classes.root}>
+            <CardMedia
+              className={classes.media}
+              image="https://live.staticflickr.com/65535/51296892783_ff5dc2707f_w.jpg"
+              title="https://www.deviantart.com/gerryarthur/art/Leaena-573272592"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Mage
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                A cunning mage, setting out on their first quest out of their
+                apprenticeship.
+              </Typography>
+            </CardContent>
+            <Container>
+              <FormField
+                onChange={handleInputChange}
+                name="mage"
+                value={formField.name}
+              />
+            </Container>
+          </Card>
+        </Grid>
+        <Grid item sm={12} className={classes.textAlignCen}>
+          <SelectButton
+            disabled={!formField.name}
+            onClick={(event) => formSubmit(event, "Warrior")}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
