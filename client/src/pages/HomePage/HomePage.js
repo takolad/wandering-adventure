6b59e31d4ed0dc8378ee77fff3084a972d64289c
John Hinojosa <hinojohn@ymail.com>
2021-07-15 23:44:29 -0500

    Home page now displays continue button if user has an active game (based off API call)

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,5 +1,4 @@
-
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
@@ -9,9 +8,7 @@ import ContinueButton from "../../components/ContinueButton";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./homepage.css";
 import ParticleBackground from "../../Particles";
-import API from '../../utils/API'
-
-
+import API from "../../utils/API";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -33,41 +30,42 @@ export default function HomePage() {
   const classes = useStyles();
   const { user } = useAuth0();
   const userId = user.sub.split("|")[1];
-
-  const [chState, setChState] = useState('')
-  
+  const [saveFlag, setSaveFlag] = useState(false);
 
   useEffect(() => {
-    console.log(userId)
-    API.getCharacters(userId)
-      .then(res => {
-        setChState('true')
-        console.log(res.data)
-      }
-      )
+    determineContinue();
+  }, []);
 
-      .catch(err => console.log(err))
-
-  }, [])
+  function determineContinue() {
+    API.getActiveGamesByUser(userId)
+      .then((res) => {
+        if (res.status === 200) {
+          setSaveFlag(true);
+        } else if (res.status === 500) {
+          console.log(res.statusText);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   return (
     <Grid container justify="center" alignItems="center">
       <Grid item sm={3} />
       <Grid item sm={6}>
-        <Card id='cardo'>
+        <Card id="cardo">
           <Container>
             <div className={classes.root}>
               <div id="center">
                 <h2>
-                  Hi {user.given_name || user.nickname || user.name}!<br/>
-                   Make a selection.
+                  Hi {user.given_name || user.nickname || user.name}! Make a
+                  selection.
                 </h2>
                 <Container>
                   <StartButton />
                   <br />
-                  {chState === 'true' ? (
-                    <ContinueButton />
-                    ) : null}
+                  {saveFlag === true ? <ContinueButton /> : ""}
                 </Container>
               </div>
             </div>
